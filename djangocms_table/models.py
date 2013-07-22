4517e605d5383ae9df7f1d6e2df8efcc5a1cbdba
Steve Holden <steve@holdenweb.com>
2013-07-22 13:25:45 -0700

    Initial changes to inject style into the interface.

--- djangocms_table/models.py
+++ djangocms_table/models.py
@@ -2,6 +2,11 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from cms.models import CMSPlugin, Page
 
+STYLE_CHOICES = (
+    ("One", "one"),
+    ("Two", "Two")
+    )
+
 class Table(CMSPlugin):
     """
     Table plugin
@@ -13,7 +18,7 @@ class Table(CMSPlugin):
     headers_left = models.PositiveSmallIntegerField(_("left"), default=0)
     headers_bottom = models.PositiveSmallIntegerField(_("bottom"), default=0)
     table_data = models.TextField(_("table data"))
-
+    style = models.CharField("Style", max_length=80, choices=STYLE_CHOICES, null=True)
 
 
     def __unicode__(self):

877647c7f7b5b601499408dcd5794b00dd7e6fd6
Steve Holden <steve@holdenweb.com>
2013-07-30 10:28:36 -0700

    Modify striping code. Allow optional JavaScript files to modify styles.

--- djangocms_table/models.py
+++ djangocms_table/models.py
@@ -2,21 +2,24 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from cms.models import CMSPlugin, Page
 
-STYLE_CHOICES = (
-    ("hor-minimalist-a", "Horizontal Minimalist A"),
-    ("hor-minimalist-b", "Horizontal Minimalist B"),
-    ("box-table-a", "Box Table A"),
-    ("box-table-b", "Box Table B"),
-    ("hor-zebra", "Horizontal Zebra"),
-    ("ver-zebra", "Vertical Zebra"),
-    ("one-column-emphasis", "One Column Emphasis"),
-    ("newspaper-a", "Newspaper A"),
-    ("newspaper-b", "Newspaper B"),
-    ("newspaper-c", "Newspaper C"),
-    ("gradient-style", "Gradient Style"),
-    ("pattern-style-a", "Pattern Style A"),
-    ("pattern-style-b", "Pattern Style B"),
+STYLE_DATA = (
+    ("hor-minimalist-a", "Horizontal Minimalist A", False),
+    ("hor-minimalist-b", "Horizontal Minimalist B", False),
+    ("box-table-a", "Box Table A", False),
+    ("box-table-b", "Box Table B", False),
+    ("hor-zebra", "Horizontal Zebra", False),
+    ("ver-zebra", "Vertical Zebra", True),
+    ("one-column-emphasis", "One Column Emphasis", False),
+    ("newspaper-a", "Newspaper A", False),
+    ("newspaper-b", "Newspaper B", False),
+    ("newspaper-c", "Newspaper C", False),
+    ("gradient-style", "Gradient Style", False),
+    ("pattern-style-a", "Pattern Style A", False),
+    ("pattern-style-b", "Pattern Style B", False),
     )
+
+STYLE_CHOICES = [(index, name) for (index, name, js) in STYLE_DATA]
+STYLE_JS = dict((index, js) for (index, name, js) in STYLE_DATA)
 
 class Table(CMSPlugin):
     """
@@ -31,6 +34,8 @@ class Table(CMSPlugin):
     table_data = models.TextField(_("table data"))
     style = models.CharField("Style", max_length=80, choices=STYLE_CHOICES, null=True)
 
+    def js(self):
+     return STYLE_JS[self.style]
 
     def __unicode__(self):
         return self.name
